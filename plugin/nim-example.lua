81dffff48f2439c4515988400fda0633a58a1f23
uga-rosa <uga6603@gmail.com>
2022-12-01 21:23:31 +0900

    display time taken

--- plugin/nim-example.lua
+++ plugin/nim-example.lua
@@ -1,13 +1,28 @@
+if vim.g.loaded_nim_example then
+  return
+end
+vim.g.loaded_nim_example = true
+
 local example = require("nim-example")
+
+---@param msg string
+---@param ... unknown
+local function printf(msg, ...)
+  print(msg:format(...))
+end
 
 vim.api.nvim_create_user_command("NimAckermann", function(opt)
   local m = tonumber(opt.fargs[1]) or 0
   local n = tonumber(opt.fargs[2]) or 0
+  local start_time = os.clock()
   print(example.nim_ackermann(m, n))
+  printf("time: %s ms", (os.clock() - start_time) * 1000)
 end, { nargs = "+" })
 
 vim.api.nvim_create_user_command("LuaAckermann", function(opt)
   local m = tonumber(opt.fargs[1]) or 0
   local n = tonumber(opt.fargs[2]) or 0
+  local start_time = os.clock()
   print(example.lua_ackermann(m, n))
+  printf("time: %s ms", (os.clock() - start_time) * 1000)
 end, { nargs = "+" })
