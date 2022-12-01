6bc6125549c9fa8f6ccedd112664bb21bc818c6d
uga-rosa <uga6603@gmail.com>
2022-12-01 10:26:39 +0900

    improve make

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
+ifeq ($(OS), Windows_NT)
+	MKD := cmd /C mkdir
+	TARGET := libfib.dll
+else
+	MKD := mkdir -p
+	TARGET := libfib.so
+endif
+
 .PHONY: build
 build:
-	nim c --app:lib -d:release src/fib.nim
+	$(MKD) build
+	nim c --app:lib -d:release -o:build/$(TARGET) src/fib.nim
--- lua/nim-example/init.lua
+++ lua/nim-example/init.lua
@@ -7,9 +7,9 @@ local this_file_path = uv.fs_realpath(debug.getinfo(1, "S").source:sub(2))
 local root_path = fn.fnamemodify(this_file_path, ":h:h:h")
 local lib_path
 if ffi.os == "Windows" then
-  lib_path = fn.expand(root_path .. "/src/fib.dll")
+  lib_path = fn.expand(root_path .. "/build/libfib.dll")
 else
-  lib_path = fn.expand(root_path .. "/src/libfib.so")
+  lib_path = fn.expand(root_path .. "/build/libfib.so")
 end
 
 local libfib = ffi.load(lib_path)
